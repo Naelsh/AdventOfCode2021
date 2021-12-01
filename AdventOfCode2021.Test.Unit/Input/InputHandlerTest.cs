b70c8544a2bdbb9529c2c1545b7c691c41d372de
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2021-12-01 13:52:22 +0100

    Added test for file input

---
+++ AdventOfCode2021.Test.Unit/Input/InputHandlerTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using AdventOfCode2021.Input;
+
+namespace AdventOfCode2021.Test.Unit.Input
+{
+    [TestFixture]
+    class InputHandlerTest
+    {
+        [Test]
+        public void GetInputFromSingleColumnFile()
+        {
+            string[] expected = { "199", "200","208","210","200","207","240","269","260","263"};
+            string filePath = "singleColumnTest.txt";
+            InputHandler inputHandler = new InputHandler(filePath);
+
+            string[] extracted = inputHandler.GetSingleColumnInputToList();
+            
+            Assert.AreEqual(expected, extracted);
+        }
+    }
+}
