e8e7b89286a0a7d7a62d281f6d46c29a4774b5ad
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2021-12-01 07:58:57 +0100

    removed wpf

---
+++ AdventOfCode2021ASP/Dec1/Day1_View.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdventOfCode2021ASP.Dec1
+{
+    /// <summary>
+    /// Interaction logic for Day1_View.xaml
+    /// </summary>
+    public partial class Day1_View : Page
+    {
+        public Day1_View()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
