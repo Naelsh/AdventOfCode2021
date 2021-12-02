8743865fb21529807c8383a9bf79ebe0d04e4b56
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2021-12-02 11:37:17 +0100

    Added movement functionality

---
+++ AdventOfCode2021.Test.Unit/Dec2/SubmarineTest.cs
@@ -0,0 +1,35 @@
+﻿using AdventOfCode2021.Dec2;
+using NUnit.Framework;
+
+namespace AdventOfCode2021.Test.Unit.Dec2
+{
+    [TestFixture]
+    class SubmarineTest
+    {
+        [TestCase(5, MovementDirection.forward, 5,0)]
+        [TestCase(5, MovementDirection.down, 0,5)]
+        [TestCase(5, MovementDirection.up, 0,-5)]
+        public void MoveSubmarineFromStart(int magnitude, MovementDirection direction, float expectedX, float expectedY)
+        {
+            Submarine submarine = new Submarine();
+            submarine.MoveSubmarine(magnitude, direction);
+
+            Assert.AreEqual(expectedX, submarine.GetPosition().X);
+            Assert.AreEqual(expectedY, submarine.GetPosition().Y);
+        }
+
+        [TestCase(5, MovementDirection.forward, 10, 5)]
+        [TestCase(5, MovementDirection.down, 5, 10)]
+        [TestCase(5, MovementDirection.up, 5, 0)]
+        public void MoveSubmarineFromPosition(int magnitude, MovementDirection direction, float expectedX, float expectedY)
+        {
+            Submarine submarine = new Submarine();
+            submarine.MoveSubmarine(5, MovementDirection.forward);
+            submarine.MoveSubmarine(5, MovementDirection.down);
+            submarine.MoveSubmarine(magnitude, direction);
+
+            Assert.AreEqual(expectedX, submarine.GetPosition().X);
+            Assert.AreEqual(expectedY, submarine.GetPosition().Y);
+        }
+    }
+}
---
+++ AdventOfCode2021/Dec2/Submarine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Dec2
+{
+    public class Submarine
+    {
+        private Vector2 _position = new Vector2(0, 0);
+
+        public Vector2 GetPosition()
+        {
+            return _position;
+        }
+
+        public void MoveSubmarine(int magnitude, MovementDirection direction)
+        {
+            switch (direction)
+            {
+                case MovementDirection.forward:
+                    _position.X += magnitude;
+                    break;
+                case MovementDirection.up:
+                    _position.Y -= magnitude;
+                    break;
+                case MovementDirection.down:
+                    _position.Y += magnitude;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
