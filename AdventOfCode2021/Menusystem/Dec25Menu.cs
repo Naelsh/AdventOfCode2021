5f4a2ec0052c663bfde9cd22db50d3225aa530fa
Naelsh/SurfacePro <niklas.o.lindblad@gmail.com>
2021-12-04 21:45:31 +0100

    Added all menus for the comming porblems

---
+++ AdventOfCode2021/Menusystem/Dec25Menu.cs
@@ -0,0 +1,71 @@
+﻿using AdventOfCode2021.Dec2;
+using AdventOfCode2021.Dec3;
+using AdventOfCode2021.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Menusystem
+{
+    class Dec25Menu : ProblemMenu
+    {
+        private Menu _mainMenu;
+
+        public Dec25Menu(Menu mainMenu)
+        {
+            _mainMenu = mainMenu;
+        }
+
+        public override void PrintResult(string result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PrintResult(int result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ShowMenu()
+        {
+            Console.WriteLine("Welcome to december 25rd. Make sure that you copy your input into a file called dec25.txt in the debug folder");
+            Console.WriteLine("Then select one of the following:");
+            Console.WriteLine("1. Problem one");
+            Console.WriteLine("2. Problem two");
+            Console.WriteLine("3. Return to main menu");
+        }
+
+        public override Menu GetNextMenu(int input)
+        {
+            if (input == 3)
+            {
+                return _mainMenu;
+            }
+            else
+            {
+                return this;
+            }
+        }
+
+        public override int HandleInput()
+        {
+            int input = GetIntAboveZeroFromUserInput(3);
+            InputHandler inputHandler = new InputHandler();
+            string[] results = inputHandler.GetInputRows("dec25.txt");
+            switch (input)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+            }
+            return input;
+        }
+    }
+}
--- AdventOfCode2021/Menusystem/MainMenu.cs
+++ AdventOfCode2021/Menusystem/MainMenu.cs
@@ -15,6 +15,28 @@ namespace AdventOfCode2021.Menusystem
             _possibleMenus.Add(1, new Dec1Menu(this));
             _possibleMenus.Add(2, new Dec2Menu(this));
             _possibleMenus.Add(3, new Dec3Menu(this));
+            _possibleMenus.Add(4, new Dec4Menu(this));
+            _possibleMenus.Add(5, new Dec5Menu(this));
+            _possibleMenus.Add(6, new Dec6Menu(this));
+            _possibleMenus.Add(7, new Dec7Menu(this));
+            _possibleMenus.Add(8, new Dec8Menu(this));
+            _possibleMenus.Add(9, new Dec9Menu(this));
+            _possibleMenus.Add(10, new Dec10Menu(this));
+            _possibleMenus.Add(11, new Dec11Menu(this));
+            _possibleMenus.Add(12, new Dec12Menu(this));
+            _possibleMenus.Add(13, new Dec13Menu(this));
+            _possibleMenus.Add(14, new Dec14Menu(this));
+            _possibleMenus.Add(15, new Dec15Menu(this));
+            _possibleMenus.Add(16, new Dec16Menu(this));
+            _possibleMenus.Add(17, new Dec17Menu(this));
+            _possibleMenus.Add(18, new Dec18Menu(this));
+            _possibleMenus.Add(19, new Dec19Menu(this));
+            _possibleMenus.Add(20, new Dec20Menu(this));
+            _possibleMenus.Add(21, new Dec21Menu(this));
+            _possibleMenus.Add(22, new Dec22Menu(this));
+            _possibleMenus.Add(23, new Dec23Menu(this));
+            _possibleMenus.Add(24, new Dec24Menu(this));
+            _possibleMenus.Add(25, new Dec25Menu(this));
         }
 
         public override void ShowMenu()
